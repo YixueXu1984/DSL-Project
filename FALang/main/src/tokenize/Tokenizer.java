6b57cdb79c201cc7f86e4adb4af169ae768cba56
noahadams <adamsnoah98@gmail.com>
2019-09-30 23:20:27 -0700

    Project Folders + draft ebnf + tokenizer

--- FALang/main/src/tokenize/Tokenizer.java
+++ FALang/main/src/tokenize/Tokenizer.java
@@ -1,4 +1,28 @@
 package tokenize;
 
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class Tokenizer {
+
+    //Basic EBNF modifications should only require changing these two static fields
+    private static String[] delimitors = {"{", "}", "(", ")", ",", ":", "->"};
+    private static String skips = " \n\r";
+
+    //helper
+    private static String wrapAll(String s) {
+        for(String d: delimitors) {
+            s = s.replace(d, " " + d + " ");
+        }
+        return s;
+    }
+
+    public static String[] tokenize(String program) {
+        StringTokenizer st = new StringTokenizer(program, skips);
+        List<String> tokens = new ArrayList<>();
+        while(st.hasMoreTokens())
+            tokens.addAll(Arrays.asList(Tokenizer.wrapAll(st.nextToken()).split(" ")));
+        return tokens.toArray(new String[tokens.size()]);
+    }
 }
